212387eb839eb0ad74414f8eb9139290e477fddf
EduerPJ <eduerpj@gmail.com>
2020-04-13 11:34:39 -0500

    Facades complete

--- app/Models/Paypal.php
+++ app/Models/Paypal.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Models;
-
 class Paypal {
     private $id;
     private $secret;
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ Route::get('/', function () {
 Route::resource('posts', 'PostController');
 Route::resource('comments', 'CommentController');
 
+/* 
+Content Servide Providers | Kernel | generando un nuevo provider | config método register | inyectrlo acá
 Route::get('/paypal', function(App\Models\Paypal $paypal){
     return $paypal->doSomething();
+}); */
+
+
+// Inyectar una clase por medio de un Facade
+// Generar un nuevo provider make:provider nameProvider y confi método register | register en app.php y add alias
+Route::get('/paypal', function(){
+    return Payment::doSomething();
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
